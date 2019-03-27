c56d6c6c5213738d2987394b5c5560569f6d0837
Marek Pavlík <ja@marekpavlik.cz>
2019-03-27 07:53:28 +0100

    deploy configuration

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:pavlikmarek/zonky-average-counter.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   devServer: {
     proxy: "https://api.zonky.cz/"
-  }
+  },
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/zonky-average-counter/" : "/"
 };
